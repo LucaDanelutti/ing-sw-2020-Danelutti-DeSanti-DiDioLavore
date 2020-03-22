25ba74946c77c996c3a08c9d46ab4be2dcc649d3
ian-ofgod <ian.didio@mail.polimi.it>
2020-03-22 12:20:17 +0100

    Created enum BlockType with the corresponding Test module

---
+++ src/main/java/it/polimi/ingsw/model/BlockType.java
@@ -0,0 +1,33 @@
+package it.polimi.ingsw.model;
+
+import java.awt.font.GlyphVector;
+
+/**
+ *<h1>BlockType</h1>
+ * This enum is used to represent the level of a block, each block will have its own "type" from one of the following:
+ * LEVEL1 -> 1, LEVEL2 -> 2, LEVEL3 -> 3 or DOME -> 4
+ */
+public enum BlockType {
+    LEVEL1((byte)1),
+    LEVEL2((byte)2),
+    LEVEL3((byte)3),
+    LEVEL4((byte)4);
+
+    private final byte level;
+
+    /**
+     * This is the constructor for the BlockType enum.
+     * @param level This is the type of the block  (1°floor->1| 2 | 3 |4 <- DOME).
+     */
+    BlockType(byte level){
+        this.level=level;
+    }
+
+    /**
+     * This function is used to retrieve the level of the block selected.
+     * @return byte This is the level of the block selected.
+     */
+    public byte getLevel(){
+        return this.level;
+    }
+}
---
+++ src/test/java/it/polimi/ingsw/model/BlockTypeTest.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.model;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * <H1>BlockTypeTest</H1>
+ * This class is used to test the BlockType enumeration
+ */
+class BlockTypeTest {
+
+    /**
+     * This function creates a BlockType object and checks that getValue returns the correct value
+     */
+    @Test
+    void getLevelTest() {
+        BlockType tester = BlockType.LEVEL1;
+        assertEquals(1, tester.getLevel(),"Should return the value of LEVEL1, which is 1");
+    }
+}
