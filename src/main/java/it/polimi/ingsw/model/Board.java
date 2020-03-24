5a93bfb2b75d8e2fe9c91aa91af0fcbb53f00c69
ian-ofgod <ian.didio@mail.polimi.it>
2020-03-24 19:28:09 +0100

    Added Board class and relative test module

---
+++ src/main/java/it/polimi/ingsw/model/Board.java
@@ -0,0 +1,95 @@
+package it.polimi.ingsw.model;
+
+
+/**
+ * <h1>Board</h1>
+ * This class represent the actual board of the concrete game.
+ * It is composed of a matrix 5x5 of cells, each one containing a stack of blocks and/or a Pawn.
+ */
+class Board{
+    private Cell[][] matrix;
+
+    /**
+     * This is the constructor for this class;
+     */
+    Board(){
+        this.matrix=new Cell[5][5];
+        for(int i=0; i<this.matrix.length; i++){
+            for(int j=0; j<this.matrix[0].length; j++){
+                this.matrix[i][j]=new Cell();
+            }
+        }
+    }
+
+    /**
+     * This is the getter for the matrix
+     * @return the variable matrix
+     */
+    Cell[][] getMatrixCopy(){
+        Cell[][] clonedMatrix=new Cell[5][5];
+        for (int i=0; i<clonedMatrix.length; i++){
+            for(int j=0; j<clonedMatrix[0].length; j++){
+                clonedMatrix[i][j]=new Cell(this.matrix[i][j]);
+            }
+        }
+        return clonedMatrix;
+    }
+
+    /**
+     * This function updates the position of the pawn inside of the board, and also updates the inside variables of the
+     * Pawn class to match accordingly.
+     * @param prevPosition previous position
+     * @param newPosition new position
+     */
+    void updatePawnPosition(Position prevPosition, Position newPosition){
+        Cell prevCell=matrix[prevPosition.getX()][prevPosition.getY()];
+        Cell newCell=matrix[newPosition.getX()][newPosition.getY()];
+        newCell.setPawn(prevCell.getPawn());
+        prevCell.setPawn(null);
+        newCell.getPawn().setPosition(newPosition);
+        newCell.getPawn().setDeltaHeight(newCell.getSize()-prevCell.getSize());
+    }
+
+    /**
+     * This function is used during the ChoosePawnPosition State of the game to set the initial position of a pawn in the Board
+     * @param pawn the pawn to be set
+     * @param position the position to set the pawn
+     */
+    void setPawnPosition(Pawn pawn, Position position){
+        pawn.setPosition(position);
+        pawn.setDeltaHeight(0);
+        matrix[position.getX()][position.getY()].setPawn(pawn);
+    }
+
+    /**
+     * This function is used when an construct action is run on the board, it pushes a new legal block on top
+     * @param constructPosition the position where the block will be constructed
+     * @param type The type of the Block to be added
+     */
+    void pawnConstruct(Position constructPosition, BlockType type){
+        matrix[constructPosition.getX()][constructPosition.getY()].pushBlock(new Block(type));
+    }
+}
+
+//    /**
+//     * This is the copy constructor for the class Board
+//     * @param toBeCopied The origin Board
+//     */
+//    Board(Board toBeCopied){
+//        this.matrix=new Cell[5][5];
+//        for (int i=0; i<5; i++){
+//            for(int j=0; j<5; j++){
+//                this.matrix[i][j]=new Cell(toBeCopied.matrix[i][j]);
+//            }
+//        }
+//    }
+
+//
+//
+//    /**
+//     * This function returns a deep copy of the board, excluding the pawns which are shallow copy
+//     * @return cloned board
+//     */
+//    Board getBoardCopy(){
+//        return new Board(this);
+//    }
---
+++ src/test/java/it/polimi/ingsw/model/BoardTest.java
@@ -0,0 +1,81 @@
+package it.polimi.ingsw.model;
+
+import org.junit.jupiter.api.Test;
+
+import javax.print.attribute.standard.PrinterIsAcceptingJobs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This is the test class for Board
+ */
+class BoardTest {
+
+    /**
+     * This is the test for the getMatrixCopy function, we test that an empty matrix is equal to a newly created board.matrix
+     */
+    @Test
+    void getMatrixCopy() {
+        Cell[][] defaultMatrix=new Cell[5][5];
+        for(int i=0; i<defaultMatrix.length; i++){
+            for(int j=0; j<defaultMatrix[0].length; j++){
+                defaultMatrix[i][j]=new Cell();
+            }
+        }
+
+        Board b =new Board();
+        Cell[][] matrixCopy=b.getMatrixCopy();
+
+        for(int i=0; i<defaultMatrix.length; i++){
+            for(int j=0; j<defaultMatrix[0].length; j++){
+                assertEquals(defaultMatrix[i][j],matrixCopy[i][j],"each cell should contain the same value");
+                assertNotSame(defaultMatrix[i][j],matrixCopy[i][j],"each cell should not be the same reference");
+            }
+        }
+
+    }
+
+    /**
+     * This is the test for the updatePawnPosition function, inside of it we test that when a pawn position is changed
+     * the variables between the previous position and the new position are switched.
+     */
+    @Test
+    void updatePawnPosition() {
+        Board tester = new Board();
+        tester.pawnConstruct(new Position(0,1), BlockType.LEVEL1);
+        tester.setPawnPosition(new Pawn(1),new Position(0,0));
+        Cell[][] original=tester.getMatrixCopy();
+        tester.updatePawnPosition(new Position(0,0), new Position(0,1));
+        Cell[][] mod=tester.getMatrixCopy();
+        assertSame(original[0][0].getPawn(),mod[0][1].getPawn(),"the same reference to the pawn should be present");
+        assertEquals(mod[0][1].getPawn().getDeltaHeight(),1,"the deltaHeight should be 1");
+    }
+
+
+    /**
+     * This is the test for the function setPawnPosition, we check that the pawn passed is the same (exact reference) to
+     * the one putted in the matrix, and that the position is set correctly in the pawn inner variables
+     */
+    @Test
+    void setPawnPosition() {
+        Board tester = new Board();
+        Pawn pwn = new Pawn(1);
+        tester.setPawnPosition(pwn,new Position(0,0));
+        Cell[][] matrix=tester.getMatrixCopy();
+        assertSame(pwn,matrix[0][0].getPawn(),"we have to set the shallow copy of the pawn");
+        assertEquals(pwn.getPosition(),new Position(0,0),"the position of the pawn should be set correctly");
+    }
+
+    /**
+     *This is the test for the function pawnConstruct, we test that given a specif type of block it is created correctly
+     */
+    @Test
+    void pawnConstruct() {
+        Board tester = new Board();
+        tester.setPawnPosition(new Pawn(1),new Position(0,0));
+        Cell[][] original=tester.getMatrixCopy();
+        tester.pawnConstruct(new Position(0,1),BlockType.LEVEL1);
+        Cell[][] mod=tester.getMatrixCopy();
+        assertEquals(BlockType.LEVEL1,mod[0][1].peekBlock().getType(),"The block constructed should be correct");
+    }
+}
