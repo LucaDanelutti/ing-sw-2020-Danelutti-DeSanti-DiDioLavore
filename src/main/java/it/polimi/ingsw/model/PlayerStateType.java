d9d608a9f5a4cb5e720c2f98d481e0c591223bac
Luca Danelutti <lucadanelutti@outlook.com>
2020-03-26 20:28:24 +0100

    Add WaitingOtherPlayers PlayerState

--- src/main/java/it/polimi/ingsw/model/PlayerStateType.java
+++ src/main/java/it/polimi/ingsw/model/PlayerStateType.java
@@ -1,12 +1,14 @@
 package it.polimi.ingsw.model;
 
 /**
- * This enum is used to represent the playerState of a player
+ * This enum is used to represent the state of a player
+ * WaitingOtherPlayers is a pre-match state
  * SelectGameCardsState, ChooseCardState, ChoosePawnsPositionState are setup states
  * IdleState, ActionState are match states
  * LoserState, WinnerState are final states
  */
 enum PlayerStateType {
+        WaitingOtherPlayers(),
         SelectGameCardsState(),
         ChooseCardState(),
         ChoosePawnsPositionState(),
