0315bff8117d4706b7b9599bbf41f8d8904d681a
Luca Danelutti <lucadanelutti@outlook.com>
2020-03-29 23:16:33 +0200

    Add signatures

--- src/main/java/it/polimi/ingsw/model/playerstate/ActionState.java
+++ src/main/java/it/polimi/ingsw/model/playerstate/ActionState.java
@@ -1,12 +1,43 @@
 package it.polimi.ingsw.model.playerstate;
 
+import it.polimi.ingsw.model.Pawn;
+import it.polimi.ingsw.model.action.Action;
+import it.polimi.ingsw.model.board.Cell;
+
+import java.util.ArrayList;
+
 /**
  * Match state ActionState: during a match the player that is currently
  * playing his turn is in this state, the other players are in IdleState
  */
 public class ActionState extends PlayerState {
-    public ActionState() {
+    private ArrayList<Action> actionList;
+    private Action currentAction;
+    private Pawn selectedPawn;
+
+    public ActionState(ArrayList<Action> actionList) {
         super(PlayerStateType.ActionState);
+        this.actionList = actionList;
     }
-    //TODO
+
+    public ArrayList<Action> getActionList() {
+        return actionList;
+    }
+
+    public void setActionList(ArrayList<Action> actionList) {
+        this.actionList = actionList;
+    }
+
+    public Pawn getSelectedPawn() {
+        return selectedPawn;
+    }
+
+    public void setSelectedPawn(Pawn selectedPawn) {
+        this.selectedPawn = selectedPawn;
+    }
+
+    public ArrayList<Pawn> availablePawns(Cell[][] matrixCopy, ArrayList<Pawn> playerPawnList) {
+        //TODO
+        return new ArrayList<Pawn>();
+    }
 }
