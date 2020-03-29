b944f4fd8c3cbf76946e3427593ee549b453b8bc
ian-ofgod <ian.didio@mail.polimi.it>
2020-03-29 12:13:19 +0200

    IdleState: added actionList and get/set

--- src/main/java/it/polimi/ingsw/model/playerstate/IdleState.java
+++ src/main/java/it/polimi/ingsw/model/playerstate/IdleState.java
@@ -1,12 +1,27 @@
 package it.polimi.ingsw.model.playerstate;
 
+import it.polimi.ingsw.model.action.Action;
+
+import java.util.ArrayList;
+
 /**
  * Match state IdleState: during a match every player is in
  * this state except the one that is currently playing his turn
  */
 public class IdleState extends PlayerState {
+
+
+    private ArrayList<Action> actionList;
     public IdleState() {
         super(PlayerStateType.IdleState);
     }
+    public ArrayList<Action> getActionList() {
+        return actionList;
+    }
+
+    public void setActionList(ArrayList<Action> actionList) {
+        this.actionList = actionList;
+    }
+
     //TODO
 }
