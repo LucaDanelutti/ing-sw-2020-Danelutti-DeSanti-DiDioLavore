8a53be13384c31227f69923bc5af8c9ac212191c
riccardodesanti <riccardodesantimail@gmail.com>
2020-04-19 12:53:49 +0200

    added test functions

--- src/test/java/it/polimi/ingsw/model/action/GeneralActionTest.java
+++ src/test/java/it/polimi/ingsw/model/action/GeneralActionTest.java
@@ -4,6 +4,7 @@ import it.polimi.ingsw.model.Pawn;
 import it.polimi.ingsw.model.Position;
 import it.polimi.ingsw.model.board.BlockType;
 import it.polimi.ingsw.model.board.Board;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -12,39 +13,64 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GeneralActionTest {
 
+    ArrayList<Position> notAvailableCellsTester;
+    Pawn selectedPawnTester;
+    Pawn notSelectedPawnTester;
+    Board boardTester;
+
+    @BeforeEach
+    void setUpTests() {
+        notAvailableCellsTester = new ArrayList<>();
+        selectedPawnTester = new Pawn("white");
+        notSelectedPawnTester = new Pawn("white");
+        boardTester = new Board();
+    }
+
     /**
-     * This test checks whether the availableCells() function of GeneralAction works properly
+     * This test checks whether the availableCells() function works properly when destroyPawnAndBuildEnable = true
      */
     @Test
-    void availableCells() {
-        ArrayList<Position> notAvailableCellsTester = new ArrayList<>();
-        GeneralAction generalActionTester = new GeneralAction(true, notAvailableCellsTester, true, false);
-
-        Pawn selectedPawnTester = new Pawn("white");
-        Pawn notSelectedPawnTester = new Pawn("white");
+    void availableCellsCheckDestroyPawnAndBuildEnable() {
+        GeneralAction generalActionTester = new GeneralAction(true, notAvailableCellsTester, false, true);
         selectedPawnTester.setPosition(new Position(1,2));
         notSelectedPawnTester.setPosition(new Position(1,1));
 
         generalActionTester.setSelectedPawn(selectedPawnTester);
         generalActionTester.setNotSelectedPawn(notSelectedPawnTester);
 
-
-        Board boardTester = new Board();
-
-//        boardTester.pawnConstruct(new Position(1,1), BlockType.DOME);
         boardTester.setPawnPosition(notSelectedPawnTester, new Position(1,1));
 
         Pawn enemyPawn = new Pawn("grey");
         boardTester.setPawnPosition(enemyPawn, new Position(2,3));
 
+        ArrayList<Position> availableCellsTester = generalActionTester.availableCells(boardTester.getMatrixCopy());
+        ArrayList<Position> expectedList = new ArrayList<>();
+        assertTrue(availableCellsTester.containsAll(expectedList) && expectedList.containsAll(availableCellsTester));
+    }
 
-//        boardTester.pawnConstruct(new Position(3,4), BlockType.DOME);
+    /**
+     * This test checks whether the availableCells() function works properly when pushEnable = true and there is a pawn that can be pushed
+     * and another pawn that cannot be pushed because it's on a border cell
+     */
+    @Test
+    void availableCellsCheckPushEnable() {
+        GeneralAction generalActionTester = new GeneralAction(true, notAvailableCellsTester, true, false);
 
+        Pawn opponentPawn1Tester = new Pawn("grey");
+        Pawn opponentPawn2Tester = new Pawn("grey");
 
+        generalActionTester.setSelectedPawn(selectedPawnTester);
+        generalActionTester.setNotSelectedPawn(notSelectedPawnTester);
+
+        boardTester.setPawnPosition(selectedPawnTester, new Position(1,1));
+        boardTester.setPawnPosition(opponentPawn1Tester, new Position(1,2));
+        boardTester.setPawnPosition(opponentPawn2Tester, new Position(0,0));
 
         ArrayList<Position> availableCellsTester = generalActionTester.availableCells(boardTester.getMatrixCopy());
-//        availableCellsTester.forEach(value -> System.out.println(value.getX() + " " + value.getY()));
-
-        assertEquals(new Position(2,3), availableCellsTester.get(0), "The position should be the same");
+        ArrayList<Position> expectedList = new ArrayList<Position>() {{
+            add(new Position(1,2));
+        }};
+        assertTrue(availableCellsTester.containsAll(expectedList) && expectedList.containsAll(availableCellsTester));
     }
+
 }
