7195331ad4463b6f5ce74be34157e0b28b78aaf9
riccardodesanti <riccardodesantimail@gmail.com>
2020-05-03 13:38:28 +0200

    minor fix

--- src/main/java/it/polimi/ingsw/view/listeners/updates/DoublePawnPositionUpdateListener.java
+++ src/main/java/it/polimi/ingsw/view/listeners/updates/DoublePawnPositionUpdateListener.java
@@ -3,5 +3,5 @@ package it.polimi.ingsw.view.listeners.updates;
 import it.polimi.ingsw.model.Position;
 
 public interface DoublePawnPositionUpdateListener {
-    public void onDoublePawnPositionUpdate(Integer workerId1, Position workerPos1, Integer workerIdPos2, Position workerPos2);
+    public void onDoublePawnPositionUpdate(Integer workerId1, Position workerPos1, Integer workerId2, Position workerPos2);
 }
