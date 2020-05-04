dde004f0904b0db0ebd9b07fc6cee415d12235ae
Luca Danelutti <lucadanelutti@outlook.com>
2020-05-04 18:50:38 +0200

    Remove useless and problematic imports

--- src/main/java/it/polimi/ingsw/utility/messages/sets/ChosenBlockTypeSetMessage.java
+++ src/main/java/it/polimi/ingsw/utility/messages/sets/ChosenBlockTypeSetMessage.java
@@ -1,9 +1,7 @@
 package it.polimi.ingsw.utility.messages.sets;
 
-import it.polimi.ingsw.model.Position;
 import it.polimi.ingsw.model.board.BlockType;
 import it.polimi.ingsw.utility.messages.Message;
-import sun.jvm.hotspot.opto.Block;
 
 import java.util.ArrayList;
 
