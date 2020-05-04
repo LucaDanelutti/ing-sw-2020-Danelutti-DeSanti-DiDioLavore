03f1a0021f46e754085f61519426436d3c8d4a07
riccardodesanti <riccardodesantimail@gmail.com>
2020-05-04 17:16:58 +0200

    changed message structure

--- src/main/java/it/polimi/ingsw/utility/messages/updates/ChosenCardUpdateMessage.java
+++ src/main/java/it/polimi/ingsw/utility/messages/updates/ChosenCardUpdateMessage.java
@@ -7,9 +7,19 @@ import java.util.ArrayList;
 
 public class ChosenCardUpdateMessage extends Message {
     CardView chosenCard;
+    String name;
 
-    public ChosenCardUpdateMessage(ArrayList<String> recipients, CardView chosenCard) {
+    public ChosenCardUpdateMessage(ArrayList<String> recipients, CardView chosenCard, String name) {
         super(recipients);
         this.chosenCard = chosenCard;
+        this.name = name;
+    }
+
+    public CardView getChosenCard() {
+        return chosenCard;
+    }
+
+    public String getName() {
+        return name;
     }
 }
