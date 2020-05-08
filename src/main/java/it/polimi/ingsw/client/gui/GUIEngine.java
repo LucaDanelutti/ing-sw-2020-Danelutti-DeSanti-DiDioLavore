9764129a10ef9c14cf406dccb6a603a7ef2832fd
riccardodesanti <riccardodesantimail@gmail.com>
2020-05-08 13:31:36 +0200

    implemented start() and showScene() methods, and fxml files loading

--- src/main/java/it/polimi/ingsw/client/gui/GUIEngine.java
+++ src/main/java/it/polimi/ingsw/client/gui/GUIEngine.java
@@ -1,31 +1,74 @@
 package it.polimi.ingsw.client.gui;
 
+import it.polimi.ingsw.view.ClientView;
 import it.polimi.ingsw.view.UserInterface;
 import it.polimi.ingsw.view.modelview.CardView;
 import it.polimi.ingsw.view.modelview.CellView;
 import it.polimi.ingsw.view.modelview.PawnView;
 import it.polimi.ingsw.view.modelview.PlayerView;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URL;
+
 public class GUIEngine extends Application implements UserInterface {
+
+    private Stage stage;
+    private ClientView clientView;
+    private GUIController currentController;
+
+    private static final String SANTORINI_STAGE_TITLE = "Santorini";
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("HBox Experiment 1");
+        stage = primaryStage;
+        clientView = new ClientView();
+        clientView.setUserInterface(this);
 
-        Label label = new Label("My Label GuiManager");
-
-        Scene scene = new Scene(label, 200, 100);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        showScene("/fxml/login.fxml");
     }
 
     @Override
     public void initialize() {
         launch();
+    }
+
+    public synchronized void showScene(String fxmlResource) {
+        if (fxmlResource == null) {
+            return;
+        }
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource(fxmlResource));
+            Scene scene = loader.load();
+
+            currentController = loader.getController();
+
+            if (stage != null) {
+                stage.hide();
+            } else {
+                stage = new Stage();
+                stage.setTitle(SANTORINI_STAGE_TITLE);
+                stage.setResizable(true);
+            }
+
+            if (currentController != null) {
+                currentController.setClientView(clientView);
+                currentController.setStage(this.stage);
+            }
+
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            //TODO: manage exception properly
+            System.out.println("Exception while loading fxml resource.");
+        }
+
     }
 
     //TODO: implement update() function within GuiController and call them within the methods below
