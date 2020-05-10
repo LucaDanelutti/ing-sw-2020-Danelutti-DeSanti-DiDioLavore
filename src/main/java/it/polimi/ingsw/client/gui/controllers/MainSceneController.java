cae287213a6c717645a124657e96c8dc264c7108
riccardodesanti <riccardodesantimail@gmail.com>
2020-05-10 20:33:57 +0200

    implemented first elements of MainSceneController

--- src/main/java/it/polimi/ingsw/client/gui/controllers/MainSceneController.java
+++ src/main/java/it/polimi/ingsw/client/gui/controllers/MainSceneController.java
@@ -1,7 +1,48 @@
 package it.polimi.ingsw.client.gui.controllers;
 
 import it.polimi.ingsw.client.gui.GUIController;
+import it.polimi.ingsw.view.modelview.ModelView;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.stage.Screen;
+
+import javax.swing.text.html.ImageView;
+import java.util.ArrayList;
 
 public class MainSceneController extends GUIController {
+
+    /* ===== FXML elements ===== */
+    @FXML
+    private Label clientPlayerNameLabel;
+    @FXML
+    private Label enemy1PlayerNameLabel;
+    @FXML
+    private Label enemy2PlayerNameLabel;
+
+
+    /* ===== FXML Properties ===== */
+
+
+    /* ===== FXML Set Up and Bindings ===== */
+   @FXML
+    public void initialize() {
+       clientPlayerNameLabel.setText("senza property");
+
+   }
+
+   public void updatePlayersName() {
+       ModelView modelView = clientView.getModelView();
+       clientPlayerNameLabel.setText(clientView.getName());
+       ArrayList<String> enemiesNames = modelView.getEnemiesNames(clientView.getName());
+       if (enemiesNames != null) {
+           if (enemiesNames.size() >= 1) enemy1PlayerNameLabel.setText(enemiesNames.get(0));
+           if (enemiesNames.size() == 2) enemy2PlayerNameLabel.setText(enemiesNames.get(1));
+       }
+   }
+
 
 }
