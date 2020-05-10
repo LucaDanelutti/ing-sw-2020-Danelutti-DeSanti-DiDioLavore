14fd9359cfdbd2fb0787613174e18b25307d4488
riccardodesanti <riccardodesantimail@gmail.com>
2020-05-10 19:35:43 +0200

    added connect() method and removed nickname field

--- src/main/java/it/polimi/ingsw/client/gui/controllers/LoginSceneController.java
+++ src/main/java/it/polimi/ingsw/client/gui/controllers/LoginSceneController.java
@@ -8,18 +8,30 @@ import javafx.scene.control.TextField;
 
 public class LoginSceneController extends GUIController {
 
+    /* ===== FXML elements ===== */
     @FXML
     private TextField hostnameTextField;
+    @FXML
+    private TextField portTextField;
 
 
-    /**
-     * Property to store the inserted hostname
-     */
+    /* ===== FXML Properties ===== */
     private StringProperty serverHostname;
+    private StringProperty port;
 
+
+    /* ===== FXML Set Up and Bindings ===== */
     @FXML
     public void initialize() {
         serverHostname = new SimpleStringProperty("");
+        port = new SimpleStringProperty("");
         hostnameTextField.textProperty().bindBidirectional(serverHostname);
+        portTextField.textProperty().bindBidirectional(port);
+    }
+
+    public void connect() {
+        System.out.println("clicked connect");
+        System.out.println(serverHostname.getValue());
+        System.out.println(port.getValue());
     }
 }
