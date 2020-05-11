e9137764778f872d9cbe9b059d8199804990e960
Luca Danelutti <lucadanelutti@outlook.com>
2020-05-11 19:18:41 +0200

    Remove MessageReceiver object

--- src/main/java/it/polimi/ingsw/view/ClientView.java
+++ src/main/java/it/polimi/ingsw/view/ClientView.java
@@ -12,7 +12,7 @@ import it.polimi.ingsw.view.modelview.PlayerView;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class ClientView implements SetsListener {
+public class ClientView implements SetsListener, RequestsAndUpdateListener {
     private String name;
     private ModelView modelView;
     private UserInterface userInterface;
@@ -28,7 +28,7 @@ public class ClientView implements SetsListener {
     //TODO: replace with set
     public ClientView(ServerConnection c) {
         this.serverConnection = c;
-        c.addListener(new MessageReceiver(this));
+        c.addListener(this);
         System.out.println("ClientView created!");
     }
 
@@ -52,123 +52,118 @@ public class ClientView implements SetsListener {
         this.name = name;
     }
 
-    private class MessageReceiver implements RequestsAndUpdateListener {
-        final ClientView clientView;
+    @Override
+    public void update(NicknameRequestMessage nicknameRequestMessage) {
+        System.out.println("What's your name?");
+        Scanner stdin = new Scanner(System.in);
+        String inputLine = stdin.nextLine();
+        this.update(new NicknameSetMessage(inputLine));
+    }
 
-        public MessageReceiver(ClientView clientView) {
-            this.clientView = clientView;
+    @Override
+    public void update(ChosenBlockTypeRequestMessage chosenBlockTypeRequestMessage) {
+
+    }
+
+    @Override
+    public void update(ChosenCardRequestMessage chosenCardRequestMessage) {
+        System.out.println("ChosenCardRequestMessage message received!");
+    }
+
+    @Override
+    public void update(ChosenPositionRequestMessage chosenPositionRequestMessage) {
+
+    }
+
+    @Override
+    public void update(FirstPlayerRequestMessage firstPlayerRequestMessage) {
+
+    }
+
+    @Override
+    public void update(InGameCardsRequestMessage inGameCardsRequestMessage) {
+
+    }
+
+    @Override
+    public void update(InitialPawnPositionRequestMessage initialPawnPositionRequestMessage) {
+
+    }
+
+    @Override
+    public void update(NumberOfPlayersRequestMessage numberOfPlayersRequestMessage) {
+        System.out.println("How many players do you want to play with?");
+        Scanner stdin = new Scanner(System.in);
+        int inputLine = stdin.nextInt();
+        this.update(new NumberOfPlayersSetMessage(inputLine));
+    }
+
+    @Override
+    public void update(SelectPawnRequestMessage selectPawnRequestMessage) {
+
+    }
+
+    @Override
+    public void update(CellUpdateMessage cellUpdateMessage) {
+        modelView.onCellUpdate(cellUpdateMessage.getPosition(), cellUpdateMessage.getBlockType());
+    }
+
+    @Override
+    public void update(ChosenCardUpdateMessage chosenCardUpdateMessage) {
+        modelView.onChosenCardUpdate(chosenCardUpdateMessage.getChosenCard(), chosenCardUpdateMessage.getName());
+    }
+
+    @Override
+    public void update(DoublePawnPositionUpdateMessage doublePawnPositionUpdateMessage) {
+        modelView.onDoublePawnPositionUpdate(doublePawnPositionUpdateMessage.getWorkerId1(), doublePawnPositionUpdateMessage.getWorkerPos1(), doublePawnPositionUpdateMessage.getWorkerId2(), doublePawnPositionUpdateMessage.getWorkerPos2());
+    }
+
+    @Override
+    public void update(PawnPositionUpdateMessage pawnPositionUpdateMessage) {
+        modelView.onPawnPositionUpdate(pawnPositionUpdateMessage.getWorkerId(), pawnPositionUpdateMessage.getWorkerPos());
+    }
+
+    @Override
+    public void update(PawnRemoveUpdateMessage pawnRemoveUpdateMessage) {
+        modelView.onPawnRemoved(pawnRemoveUpdateMessage.getWorkerId());
+    }
+
+
+    @Override
+    public void update(SelectedPawnUpdateMessage selectedPawnUpdateMessage) {
+        modelView.onSelectPawnUpdate(selectedPawnUpdateMessage.getWorkerId());
+    }
+
+    @Override
+    public void update(GameStartMessage gameStartMessage) {
+        for(PlayerView p : gameStartMessage.getInGamePlayers()){
+            modelView.onPlayerUpdate(p.getName(), p.getPawnList().get(0).getColor(), p.getPawnList().get(0).getId(), p.getPawnList().get(1).getId());
         }
+        System.out.println("GameStartMessage message received!");
+    }
 
-        @Override
-        public void update(NicknameRequestMessage nicknameRequestMessage) {
-            System.out.println("What's your name?");
-            Scanner stdin = new Scanner(System.in);
-            String inputLine = stdin.nextLine();
-            clientView.update(new NicknameSetMessage(inputLine));
-        }
+    @Override
+    public void update(TurnEndedMessage turnEndedMessage) {
 
-        @Override
-        public void update(ChosenBlockTypeRequestMessage chosenBlockTypeRequestMessage) {
+    }
 
-        }
+    @Override
+    public void update(YouLostMessage youLostMessage) {
 
-        @Override
-        public void update(ChosenCardRequestMessage chosenCardRequestMessage) {
-            System.out.println("ChosenCardRequestMessage message received!");
-        }
+    }
 
-        @Override
-        public void update(ChosenPositionRequestMessage chosenPositionRequestMessage) {
+    @Override
+    public void update(YouWonMessage youWonMessage) {
 
-        }
+    }
 
-        @Override
-        public void update(FirstPlayerRequestMessage firstPlayerRequestMessage) {
+    @Override
+    public void update(gameStartedAndYouAreNotSelectedMessage gameStartedAndYouAreNotSelectedMessage) {
+    }
 
-        }
+    @Override
+    public void update(YouLostAndSomeoneWonMessage youLostAndSomeoneWonMessage) {
 
-        @Override
-        public void update(InGameCardsRequestMessage inGameCardsRequestMessage) {
-
-        }
-
-        @Override
-        public void update(InitialPawnPositionRequestMessage initialPawnPositionRequestMessage) {
-
-        }
-
-        @Override
-        public void update(NumberOfPlayersRequestMessage numberOfPlayersRequestMessage) {
-            System.out.println("How many players do you want to play with?");
-        }
-
-        @Override
-        public void update(SelectPawnRequestMessage selectPawnRequestMessage) {
-
-        }
-
-        @Override
-        public void update(CellUpdateMessage cellUpdateMessage) {
-            modelView.onCellUpdate(cellUpdateMessage.getPosition(), cellUpdateMessage.getBlockType());
-        }
-
-        @Override
-        public void update(ChosenCardUpdateMessage chosenCardUpdateMessage) {
-            modelView.onChosenCardUpdate(chosenCardUpdateMessage.getChosenCard(), chosenCardUpdateMessage.getName());
-        }
-
-        @Override
-        public void update(DoublePawnPositionUpdateMessage doublePawnPositionUpdateMessage) {
-            modelView.onDoublePawnPositionUpdate(doublePawnPositionUpdateMessage.getWorkerId1(), doublePawnPositionUpdateMessage.getWorkerPos1(), doublePawnPositionUpdateMessage.getWorkerId2(), doublePawnPositionUpdateMessage.getWorkerPos2());
-        }
-
-        @Override
-        public void update(PawnPositionUpdateMessage pawnPositionUpdateMessage) {
-            modelView.onPawnPositionUpdate(pawnPositionUpdateMessage.getWorkerId(), pawnPositionUpdateMessage.getWorkerPos());
-        }
-
-        @Override
-        public void update(PawnRemoveUpdateMessage pawnRemoveUpdateMessage) {
-            modelView.onPawnRemoved(pawnRemoveUpdateMessage.getWorkerId());
-        }
-
-
-        @Override
-        public void update(SelectedPawnUpdateMessage selectedPawnUpdateMessage) {
-            modelView.onSelectPawnUpdate(selectedPawnUpdateMessage.getWorkerId());
-        }
-
-        @Override
-        public void update(GameStartMessage gameStartMessage) {
-            for(PlayerView p : gameStartMessage.getInGamePlayers()){
-                modelView.onPlayerUpdate(p.getName(), p.getPawnList().get(0).getColor(), p.getPawnList().get(0).getId(), p.getPawnList().get(1).getId());
-            }
-            System.out.println("GameStartMessage message received!");
-        }
-
-        @Override
-        public void update(TurnEndedMessage turnEndedMessage) {
-
-        }
-
-        @Override
-        public void update(YouLostMessage youLostMessage) {
-
-        }
-
-        @Override
-        public void update(YouWonMessage youWonMessage) {
-
-        }
-
-        @Override
-        public void update(gameStartedAndYouAreNotSelectedMessage gameStartedAndYouAreNotSelectedMessage) {
-        }
-
-        @Override
-        public void update(YouLostAndSomeoneWonMessage youLostAndSomeoneWonMessage) {
-
-        }
     }
 
     @Override
@@ -208,7 +203,7 @@ public class ClientView implements SetsListener {
 
     @Override
     public void update(NumberOfPlayersSetMessage numberOfPlayersSetMessage) {
-
+        serverConnection.asyncSend(numberOfPlayersSetMessage);
     }
 
     @Override
