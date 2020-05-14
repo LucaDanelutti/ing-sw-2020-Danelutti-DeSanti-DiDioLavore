737ee516bb8246a41e8c68069201682622663783
riccardodesanti <riccardodesantimail@gmail.com>
2020-05-14 11:14:53 +0200

    improved main scene fxml and controller

--- src/main/java/it/polimi/ingsw/client/gui/controllers/MainSceneController.java
+++ src/main/java/it/polimi/ingsw/client/gui/controllers/MainSceneController.java
@@ -1,21 +1,40 @@
 package it.polimi.ingsw.client.gui.controllers;
 
 import it.polimi.ingsw.client.gui.GUIController;
+import it.polimi.ingsw.model.board.BlockType;
+import it.polimi.ingsw.view.modelview.CardView;
+import it.polimi.ingsw.view.modelview.CellView;
 import it.polimi.ingsw.view.modelview.ModelView;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Screen;
+import javafx.scene.image.ImageView;
 
-import javax.swing.text.html.ImageView;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class MainSceneController extends GUIController {
 
+    private static final double BOARD_PADDING = 12.0;
+    private static final double BOARD_PADDING_RATIO = 0.76;
+
     /* ===== FXML elements ===== */
+    @FXML
+    private GridPane mainGridPane;
+
+    @FXML
+    private GridPane boardGridPane;
+
     @FXML
     private Label clientPlayerNameLabel;
     @FXML
@@ -23,15 +42,60 @@ public class MainSceneController extends GUIController {
     @FXML
     private Label enemy2PlayerNameLabel;
 
+    @FXML
+    private ImageView clientPlayerCardImageView;
+    @FXML
+    private ImageView enemy1PlayerCardImageView;
+    @FXML
+    private ImageView enemy2PlayerCardImageView;
+
+
 
     /* ===== FXML Properties ===== */
-
 
     /* ===== FXML Set Up and Bindings ===== */
    @FXML
     public void initialize() {
-       clientPlayerNameLabel.setText("senza property");
+//       new Insets(boardGridPane.widthProperty().getValue()*BOARD_PADDING))
+       boardGridPane.setPadding(new Insets(boardGridPane.widthProperty().getValue()*BOARD_PADDING));
+       System.out.println(boardGridPane.widthProperty().getValue()*BOARD_PADDING);
+//       boardGridPane.paddingProperty().bind(Bindings.createObjectBinding(() -> new Insets(boardGridPane.widthProperty().getValue()*BOARD_PADDING)));
+       System.out.println(boardGridPane.paddingProperty().getValue());
+       clientPlayerCardImageView.fitWidthProperty().bind(mainGridPane.widthProperty().divide(6));
+       clientPlayerCardImageView.fitHeightProperty().bind(mainGridPane.heightProperty().divide(2.5));
+       enemy1PlayerCardImageView.fitWidthProperty().bind(mainGridPane.widthProperty().divide(12));
+       enemy1PlayerCardImageView.fitHeightProperty().bind(mainGridPane.heightProperty().divide(5));
+       enemy2PlayerCardImageView.fitWidthProperty().bind(mainGridPane.widthProperty().divide(12));
+       enemy2PlayerCardImageView.fitHeightProperty().bind(mainGridPane.heightProperty().divide(5));
 
+   }
+
+
+   public void buildMainScene() {
+       stage.setMinWidth(Screen.getPrimary().getBounds().getWidth() / 2);
+       stage.setMinHeight(Screen.getPrimary().getBounds().getHeight() / 2);
+
+       updatePlayersName();
+       updatePlayersCard();
+       updateBoard(); //TODO: has to be remove from here
+   }
+
+
+   public void updateBoard() {
+       ModelView modelView = clientView.getModelView();
+       for(int i = 0; i < modelView.getMatrix().length; i++){
+           for(int  j = 0; j < modelView.getMatrix()[0].length; j++){
+               if (modelView.getMatrix()[i][j].getPeek() != BlockType.TERRAIN) {
+                   System.out.println("blockType: " + modelView.getMatrix()[i][j].getPeek().getLevel());
+                   Image blockImage = new Image("images/board/block_lv_" + modelView.getMatrix()[i][j].getPeek().getLevel() +".png");
+                   ImageView blockImageView = new ImageView(blockImage);
+                   blockImageView.setPreserveRatio(true);
+                   blockImageView.fitWidthProperty().bind(boardGridPane.widthProperty().divide(5).multiply(BOARD_PADDING_RATIO));
+                   blockImageView.fitHeightProperty().bind(boardGridPane.heightProperty().divide(5).multiply(BOARD_PADDING_RATIO));
+                   boardGridPane.add(blockImageView, i, j);
+               }
+           }
+       }
    }
 
    public void updatePlayersName() {
@@ -41,6 +105,28 @@ public class MainSceneController extends GUIController {
        if (enemiesNames != null) {
            if (enemiesNames.size() >= 1) enemy1PlayerNameLabel.setText(enemiesNames.get(0));
            if (enemiesNames.size() == 2) enemy2PlayerNameLabel.setText(enemiesNames.get(1));
+       }
+   }
+
+   public void updatePlayersCard() {
+       ModelView modelView = clientView.getModelView();
+
+       //gets the Card of the client player from the modelView and renders the proper Image, Name and Description
+       CardView clientPlayerCard = modelView.getClientPlayerCard(clientView.getName());
+       if (clientPlayerCard != null) {
+           Image clientPlayerCardImage = new Image("images/cards/card_" + clientPlayerCard.getId() + ".png");
+           clientPlayerCardImageView.setImage(clientPlayerCardImage);
+       }
+       ArrayList<CardView> enemiesCards = modelView.getEnemiesCards(clientView.getName());
+       if (enemiesCards != null) {
+           if (enemiesCards.size() >= 1)  {
+               Image enemy1CardImage = new Image("images/cards/card_" + enemiesCards.get(0).getId() + ".png");
+               enemy1PlayerCardImageView.setImage(enemy1CardImage); //enemiesCards.get(0)
+           }
+           if (enemiesCards.size() == 2) {
+               Image enemy2CardImage = new Image("images/cards/card_" + enemiesCards.get(1).getId() + ".png");
+               enemy2PlayerCardImageView.setImage(enemy2CardImage);
+           }
        }
    }
 
