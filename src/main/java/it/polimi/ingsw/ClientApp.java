5cce8d90283f454cc85da3e569ae43cb2a282134
Luca Danelutti <lucadanelutti@outlook.com>
2020-05-15 18:46:07 +0200

    Fix

--- src/main/java/it/polimi/ingsw/ClientApp.java
+++ src/main/java/it/polimi/ingsw/ClientApp.java
@@ -11,11 +11,11 @@ public class ClientApp
     public static void main(String[] args) {
 
         String selectedUserInterface = args.length > 0 ? args[0] : null;
-        String hostname = args.length > 0 ? args[1] : null;
-        String port = args.length > 0 ? args[2] : null;
+        String hostname = args.length > 1 ? args[1] : null;
+        String port = args.length > 2 ? args[2] : null;
 
         UserInterface userInterface;
-        if (selectedUserInterface.equals("gui")) {
+        if (selectedUserInterface != null && selectedUserInterface.equals("gui")) {
             userInterface = new GUIEngine();
         } else {
             userInterface = new CLIEngine();
