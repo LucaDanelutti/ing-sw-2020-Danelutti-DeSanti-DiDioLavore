3f1f64751440647df1f8a2c3398848b4afd30493
ian-ofgod <ian.didio@mail.polimi.it>
2020-05-16 13:11:18 +0200

    Added support for undo TimerPrinter

--- src/main/java/it/polimi/ingsw/client/cli/CLIEngine.java
+++ src/main/java/it/polimi/ingsw/client/cli/CLIEngine.java
@@ -9,7 +9,7 @@ import it.polimi.ingsw.view.modelview.CardView;
 import it.polimi.ingsw.view.modelview.CellView;
 import it.polimi.ingsw.view.modelview.PawnView;
 import it.polimi.ingsw.view.modelview.PlayerView;
-import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
@@ -18,16 +18,8 @@ import java.util.Set;
 public class CLIEngine implements UserInterface {
     private ClientView clientView;
 
-    public ClientView getClientView() {
-        return clientView;
-    }
-    public void setClientView(ClientView clientView) {
-        this.clientView = clientView;
-    }
-
     public CLIEngine() {
     }
-
     public CLIEngine(ClientView clientView) {
         this.clientView = clientView;
     }
@@ -59,13 +51,16 @@ public class CLIEngine implements UserInterface {
         arrayList.add(BlockType.DOME);
         cliEngine.onChosenBlockTypeRequest(arrayList);*/
 
-        ArrayList<CardView> arrayList = new ArrayList<>();
+        /*ArrayList<CardView> arrayList = new ArrayList<>();
         arrayList.add(new CardView(1,"Apollo","ciao ciao ciao"));
         arrayList.add(new CardView(2,"Dimetrio","cdsfsdfsdf fevdsvsf sfdv"));
         arrayList.add(new CardView(3,"Medusa","fsdfggfdgfds  sgfd sfdg "));
         arrayList.add(new CardView(4,"LALA","fsdfggfdgfds  sgfd sfdg "));
-        cliEngine.onInGameCardsRequest(arrayList);
+        cliEngine.onInGameCardsRequest(arrayList);*/
+
+        cliEngine.undoOrConfirmHandler(9);
     }
+
 
     @Override public void initialize() {
         clientView = new ClientView();
@@ -81,20 +76,18 @@ public class CLIEngine implements UserInterface {
         //call function of ClientView
         clientView.startServerConnection(serverIP, serverPORT);
     }
-
-    @Override
-    public void quickInitialize(String hostname, int port) {
+    @Override public void quickInitialize(String hostname, int port) {
         clientView = new ClientView();
         clientView.setUserInterface(this);
         clientView.startServerConnection(hostname, port);
     }
+
 
     @Override public void refreshView() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
         printCompleteGameStatus();
     }
-
     @Override public void refreshView(PawnView pawnView) {
         System.out.print("\033[H\033[2J");
         System.out.flush();
@@ -119,6 +112,7 @@ public class CLIEngine implements UserInterface {
         System.out.flush();
         printCompleteGameStatus();
     }
+
 
     @Override public void onChosenBlockTypeRequest(ArrayList<BlockType> availableBlockTypes) {
         System.out.print("\033[H\033[2J");
@@ -155,7 +149,16 @@ public class CLIEngine implements UserInterface {
             }
         }while (!isTheOptionValid(options,input));
 
-        clientView.update(new ChosenBlockTypeSetMessage(availableBlockTypes.get(input)));
+        int undoChoice = undoOrConfirmHandler(5);
+        if(undoChoice==0){
+            clientView.update(new ChosenBlockTypeSetMessage(availableBlockTypes.get(input)));
+        }
+        else if(undoChoice==1){
+            clientView.update(new UndoActionSetMessage());
+        }
+        else{
+            clientView.update(new UndoTurnSetMessage());
+        }
     }
     @Override public void onChosenCardRequest(ArrayList<CardView> availableCards) {
         System.out.print("\033[H\033[2J");
@@ -185,7 +188,49 @@ public class CLIEngine implements UserInterface {
         System.out.flush();
         printCompleteGameStatus();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Select the position from the ones below:");
+        System.out.println("MOVE -> Select the position from the ones below:");
+        for(int i=0; i<availablePositions.size(); i++){
+            if(i%5==0&&i>0){
+                System.out.println();
+            }
+            if(availablePositions.get(i)==null){
+                System.out.print(i + ") SKIP ");
+            }else {
+                System.out.print(i + ") x:" + availablePositions.get(i).getX() + " y:" + availablePositions.get(i).getY()+"   ");
+            }
+        }
+        System.out.println();
+        System.out.print("Choice (0->"+(availablePositions.size()-1)+"): ");
+        ArrayList<Integer> options = new ArrayList<>();
+        int input;
+        for(int i=0; i<availablePositions.size(); i++){
+            options.add(i);
+        }
+        do {
+            input = scanner.nextInt();
+            if(!isTheOptionValid(options,input)){
+                System.out.print("Not a valid option, retry: ");
+            }
+        }while (!isTheOptionValid(options,input));
+
+        int undoChoice = undoOrConfirmHandler(5);
+        if(undoChoice==0){
+            clientView.update(new ChosenPositionSetMessage(availablePositions.get(input)));
+        }
+        else if(undoChoice==1){
+            clientView.update(new UndoActionSetMessage());
+        }
+        else{
+            clientView.update(new UndoTurnSetMessage());
+        }
+
+    }
+    @Override public void onChosenPositionForConstructRequest(ArrayList<Position> availablePositions) {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        printCompleteGameStatus();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("CONSTRUCT -> Select the position from the ones below:");
         for(int i=0; i<availablePositions.size(); i++){
             if(i%5==0&&i>0){
                 System.out.println();
@@ -211,12 +256,6 @@ public class CLIEngine implements UserInterface {
         }while (!isTheOptionValid(options,input));
         clientView.update(new ChosenPositionSetMessage(availablePositions.get(input)));
     }
-
-    @Override
-    public void onChosenPositionForConstructRequest(ArrayList<Position> availablePositions) {
-
-    }
-
     @Override public void onFirstPlayerRequest() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
@@ -398,6 +437,7 @@ public class CLIEngine implements UserInterface {
         clientView.update(new UndoTurnSetMessage());
     }
 
+
     private boolean isThereAnyPawnOnTheBoard(){
         CellView[][] matrix=clientView.getModelView().getMatrix();
         for(int i=0; i<matrix.length; i++){
@@ -449,7 +489,6 @@ public class CLIEngine implements UserInterface {
     private void printSingleScoreRow(){
         System.out.println("---------------------------------------------------");
     }
-
     public void printBoard(){
         CellView[][] matrix=clientView.getModelView().getMatrix();
         PawnView p;
@@ -574,4 +613,63 @@ public class CLIEngine implements UserInterface {
         System.out.println("==============================================================");
     }
 
+    public int undoOrConfirmHandler(int timeToWait){
+        Scanner s= new Scanner(System.in);
+        int input = 0;
+        Thread timer = new Thread(new TimerPrinter(timeToWait));
+        timer.start();
+        do{
+            if(timer.isAlive()) {
+                input = s.nextInt();
+
+                if (input == 0 || input == 1 || input == 2) {
+                    timer.interrupt();
+                }
+            }
+        }while (!(input == 0 || input == 1 || input == 2) && timer.isAlive());
+
+        return input;
+    }
+
+    public ClientView getClientView() {
+        return clientView;
+    }
+    public void setClientView(ClientView clientView) {
+        this.clientView = clientView;
+    }
+
 }
+
+class TimerPrinter implements Runnable{
+    int timerLength;
+
+    TimerPrinter(int timerLength){
+        this.timerLength=timerLength;
+    }
+
+    private void clearConsole(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    @Override
+    public void run() {
+        long startTime=System.currentTimeMillis(),elapsedTime,elapsedSeconds;
+        int seconds=0;
+        int lastValue=-1;
+
+        while(!Thread.currentThread().isInterrupted() && seconds<timerLength){
+            elapsedTime=System.currentTimeMillis()-startTime;
+            seconds= (int)(elapsedTime/1000);
+
+            if(lastValue!=seconds) {
+                clearConsole();
+                System.out.println("Press: 0 -> CONFIRM  |  1 -> UNDO ACTION  |  2 -> UNDO TURN");
+                System.out.println("If no key is pressed, the action will be CONFIRMED  |  # " + (timerLength - seconds) + "s remaining #");
+                System.out.print("Choice: ");
+                lastValue=seconds;
+            }
+        }
+    }
+}
+
