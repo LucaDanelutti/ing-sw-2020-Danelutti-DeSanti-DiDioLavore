ba0a420372cf0d6b85b3df63d01200e1f4df0050
Luca Danelutti <lucadanelutti@outlook.com>
2020-05-20 12:40:17 +0200

    Improve JavaDoc

--- src/main/java/it/polimi/ingsw/view/ClientView.java
+++ src/main/java/it/polimi/ingsw/view/ClientView.java
@@ -2,7 +2,6 @@ package it.polimi.ingsw.view;
 
 import it.polimi.ingsw.client.Client;
 import it.polimi.ingsw.client.ServerConnection;
-import it.polimi.ingsw.client.cli.CLIEngine;
 import it.polimi.ingsw.utility.messages.requests.*;
 import it.polimi.ingsw.utility.messages.sets.*;
 import it.polimi.ingsw.utility.messages.updates.*;
@@ -13,12 +12,21 @@ import it.polimi.ingsw.view.modelview.PlayerView;
 
 import java.io.IOException;
 
+/**
+ * ClientView class: main client object. It contains the modelView that has the actual data,
+ * the userInterface and the reference to the network handler.
+ * It observes the network handler for requests and updates and the userInterface for sets.
+ */
 public class ClientView implements SetsListener, RequestsAndUpdateListener {
     private String name;
     private ModelView modelView;
     private UserInterface userInterface;
     private ServerConnection serverConnection;
 
+    /**
+     * Default constructor
+     * It creates an empty modelView
+     */
     public ClientView() {
         this.name = null;
         this.modelView = new ModelView();
@@ -26,12 +34,16 @@ public class ClientView implements SetsListener, RequestsAndUpdateListener {
         this.serverConnection = null;
     }
 
+    /**
+     * @param hostname
+     * @param port
+     * This method starts the connection to the server
+     */
     public void startServerConnection(String hostname, int port) {
         ClientView clientView = this;
         new Thread(new Runnable() {
             @Override
             public void run() {
-                modelView = new ModelView();
                 Client client = new Client(hostname, port);
                 serverConnection = client;
                 try{
@@ -43,100 +55,167 @@ public class ClientView implements SetsListener, RequestsAndUpdateListener {
         }).start();
     }
 
+    /**
+     * @return userInterface
+     * Default get method for userInterface
+     */
     public UserInterface getUserInterface() {
         return userInterface;
     }
 
+    /**
+     * @return modelView
+     * Default get method for modelView
+     */
     public ModelView getModelView() {
         return modelView;
     }
 
+    /**
+     * @param userInterface
+     * Default set method for userInterface
+     */
     public void setUserInterface(UserInterface userInterface) {
         this.userInterface = userInterface;
     }
 
+    /**
+     * @return name
+     * Default get method for name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @param name
+     * Default set method for name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * @param nicknameRequestMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     */
     @Override
     public void update(NicknameRequestMessage nicknameRequestMessage) {
-        //System.out.println("What's your name?"); //TODO: remove
-        //Scanner stdin = new Scanner(System.in);
-        //String inputLine = stdin.nextLine();
-        //this.update(new NicknameSetMessage(inputLine));
         userInterface.onNicknameRequest();
     }
 
+    /**
+     * @param chosenBlockTypeRequestMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     */
     @Override
     public void update(ChosenBlockTypeRequestMessage chosenBlockTypeRequestMessage) {
         userInterface.onChosenBlockTypeRequest(chosenBlockTypeRequestMessage.getAvailableBlockTypes());
     }
 
+    /**
+     * @param chosenCardRequestMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     */
     @Override
     public void update(ChosenCardRequestMessage chosenCardRequestMessage) {
         userInterface.onChosenCardRequest(chosenCardRequestMessage.getAvailableCards());
     }
 
+    /**
+     * @param chosenPositionForMoveRequestMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     */
     @Override
     public void update(ChosenPositionForMoveRequestMessage chosenPositionForMoveRequestMessage) {
         userInterface.onChosenPositionForMoveRequest(chosenPositionForMoveRequestMessage.getAvailablePositions());
     }
 
+    /**
+     * @param chosenPositionForConstructRequestMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     */
     @Override
     public void update(ChosenPositionForConstructRequestMessage chosenPositionForConstructRequestMessage) {
         userInterface.onChosenPositionForConstructRequest(chosenPositionForConstructRequestMessage.getAvailablePositions());
     }
 
+    /**
+     * @param firstPlayerRequestMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     */
     @Override
     public void update(FirstPlayerRequestMessage firstPlayerRequestMessage) {
         userInterface.onFirstPlayerRequest();
     }
 
+    /**
+     * @param inGameCardsRequestMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     * and updates the modelView with the received data
+     */
     @Override
     public void update(InGameCardsRequestMessage inGameCardsRequestMessage) {
-        //System.out.println("inGameCardsRequestMessage message received!"); //TODO: remove
         for(PlayerView p : inGameCardsRequestMessage.getInGamePlayers()){
             modelView.onPlayerUpdate(p.getName(), p.getPawnList().get(0).getColor(), p.getPawnList().get(0).getId(), p.getPawnList().get(1).getId());
         }
         userInterface.onInGameCardsRequest(inGameCardsRequestMessage.getAvailableCards());
     }
 
+    /**
+     * @param initialPawnPositionRequestMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     */
     @Override
     public void update(InitialPawnPositionRequestMessage initialPawnPositionRequestMessage) {
         userInterface.onInitialPawnPositionRequest(initialPawnPositionRequestMessage.getAvailablePositions());
     }
 
+    /**
+     * @param numberOfPlayersRequestMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     */
     @Override
     public void update(NumberOfPlayersRequestMessage numberOfPlayersRequestMessage) {
-        //System.out.println("How many players do you want to play with?"); //TODO: remove
-        //Scanner stdin = new Scanner(System.in);
-        //int inputLine = stdin.nextInt();
-        //this.update(new NumberOfPlayersSetMessage(inputLine));
         userInterface.onNumberOfPlayersRequest();
     }
 
+    /**
+     * @param selectPawnRequestMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     */
     @Override
     public void update(SelectPawnRequestMessage selectPawnRequestMessage) {
         userInterface.onSelectPawnRequest(selectPawnRequestMessage.getAvailablePositions());
     }
 
+    /**
+     * @param cellUpdateMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     * and updates the modelView with the received data
+     */
     @Override
     public void update(CellUpdateMessage cellUpdateMessage) {
         modelView.onCellUpdate(cellUpdateMessage.getPosition(), cellUpdateMessage.getBlockType());
         userInterface.refreshView(modelView.getMatrix()[cellUpdateMessage.getPosition().getX()][cellUpdateMessage.getPosition().getY()]);
     }
 
+    /**
+     * @param chosenCardUpdateMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     * and updates the modelView with the received data
+     */
     @Override
     public void update(ChosenCardUpdateMessage chosenCardUpdateMessage) {
         modelView.onChosenCardUpdate(chosenCardUpdateMessage.getChosenCard(), chosenCardUpdateMessage.getName());
         userInterface.refreshView(chosenCardUpdateMessage.getChosenCard());
     }
 
+    /**
+     * @param doublePawnPositionUpdateMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     * and updates the modelView with the received data
+     */
     @Override
     public void update(DoublePawnPositionUpdateMessage doublePawnPositionUpdateMessage) {
         modelView.onDoublePawnPositionUpdate(doublePawnPositionUpdateMessage.getWorkerId1(), doublePawnPositionUpdateMessage.getWorkerPos1(), doublePawnPositionUpdateMessage.getWorkerId2(), doublePawnPositionUpdateMessage.getWorkerPos2());
@@ -144,24 +223,44 @@ public class ClientView implements SetsListener, RequestsAndUpdateListener {
         userInterface.refreshView(modelView.getPawn(doublePawnPositionUpdateMessage.getWorkerId2()));
     }
 
+    /**
+     * @param pawnPositionUpdateMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     * and updates the modelView with the received data
+     */
     @Override
     public void update(PawnPositionUpdateMessage pawnPositionUpdateMessage) {
         modelView.onPawnPositionUpdate(pawnPositionUpdateMessage.getWorkerId(), pawnPositionUpdateMessage.getWorkerPos());
         userInterface.refreshView(modelView.getPawn(pawnPositionUpdateMessage.getWorkerId()));
     }
 
+    /**
+     * @param pawnRemoveUpdateMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     * and updates the modelView with the received data
+     */
     @Override
     public void update(PawnRemoveUpdateMessage pawnRemoveUpdateMessage) {
         modelView.onPawnRemoved(pawnRemoveUpdateMessage.getWorkerId());
         userInterface.refreshView();
     }
 
+    /**
+     * @param selectedPawnUpdateMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     * and updates the modelView with the received data
+     */
     @Override
     public void update(SelectedPawnUpdateMessage selectedPawnUpdateMessage) {
         modelView.onSelectPawnUpdate(selectedPawnUpdateMessage.getWorkerId());
         userInterface.refreshView(modelView.getPawn(selectedPawnUpdateMessage.getWorkerId()));
     }
 
+    /**
+     * @param gameStartMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     * and updates the modelView with the received data
+     */
     @Override
     public void update(GameStartMessage gameStartMessage) {
         for(PlayerView p : gameStartMessage.getInGamePlayers()){
@@ -170,17 +269,31 @@ public class ClientView implements SetsListener, RequestsAndUpdateListener {
         userInterface.refreshView();
     }
 
+    /**
+     * @param turnEndedMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     */
     @Override
     public void update(TurnEndedMessage turnEndedMessage) {
         userInterface.refreshView();
     }
 
+    /**
+     * @param youLostMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     * and updates the modelView with the received data
+     */
     @Override
     public void update(YouLostMessage youLostMessage) {
         modelView.setLooser(name);
         userInterface.refreshView();
     }
 
+    /**
+     * @param youWonMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     * and updates the modelView with the received data
+     */
     @Override
     public void update(YouWonMessage youWonMessage) {
         modelView.setWinner(name);
@@ -192,75 +305,134 @@ public class ClientView implements SetsListener, RequestsAndUpdateListener {
         //TODO
     }
 
+    /**
+     * @param gameEndedMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     */
     @Override
     public void update(GameEndedMessage gameEndedMessage) {
         userInterface.onGameEnded("A player disconnected!");
         serverConnection.closeConnection();
     }
 
+    /**
+     * @param undoUpdateMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     * and updates the modelView with the received data
+     */
     @Override
     public void update(UndoUpdateMessage undoUpdateMessage) {
         modelView = undoUpdateMessage.getRestoredModelView();
         userInterface.refreshView();
     }
 
+    /**
+     * @param youLostAndSomeoneWonMessage
+     * Update method to observe the network handler. It forwards the request to the userInterface
+     * and updates the modelView with the received data
+     */
     @Override
     public void update(YouLostAndSomeoneWonMessage youLostAndSomeoneWonMessage) {
         modelView.setWinner(youLostAndSomeoneWonMessage.getWinnerName());
         userInterface.onYouLostAndSomeOneWon(youLostAndSomeoneWonMessage.getWinnerName());
     }
 
+    /**
+     * @param chosenBlockTypeSetMessage
+     * Update method to observe the userInterface. It forwards the request to the network handler
+     */
     @Override
     public void update(ChosenBlockTypeSetMessage chosenBlockTypeSetMessage) {
         serverConnection.asyncSend(chosenBlockTypeSetMessage);
     }
 
+    /**
+     * @param chosenCardSetMessage
+     * Update method to observe the userInterface. It forwards the request to the network handler
+     */
     @Override
     public void update(ChosenCardSetMessage chosenCardSetMessage) {
         serverConnection.asyncSend(chosenCardSetMessage);
     }
 
+    /**
+     * @param chosenPositionSetMessage
+     * Update method to observe the userInterface. It forwards the request to the network handler
+     */
     @Override
     public void update(ChosenPositionSetMessage chosenPositionSetMessage) {
         serverConnection.asyncSend(chosenPositionSetMessage);
     }
 
+    /**
+     * @param firstPlayerSetMessage
+     * Update method to observe the userInterface. It forwards the request to the network handler
+     */
     @Override
     public void update(FirstPlayerSetMessage firstPlayerSetMessage) {
         serverConnection.asyncSend(firstPlayerSetMessage);
     }
 
+    /**
+     * @param inGameCardsSetMessage
+     * Update method to observe the userInterface. It forwards the request to the network handler
+     */
     @Override
     public void update(InGameCardsSetMessage inGameCardsSetMessage) {
         serverConnection.asyncSend(inGameCardsSetMessage);
     }
 
+    /**
+     * @param initialPawnPositionSetMessage
+     * Update method to observe the userInterface. It forwards the request to the network handler
+     */
     @Override
     public void update(InitialPawnPositionSetMessage initialPawnPositionSetMessage) {
         serverConnection.asyncSend(initialPawnPositionSetMessage);
     }
 
+    /**
+     * @param nicknameSetMessage
+     * Update method to observe the userInterface. It forwards the request to the network handler
+     * and sets the clientView name
+     */
     @Override
     public void update(NicknameSetMessage nicknameSetMessage) {
         this.setName(nicknameSetMessage.getName());
         serverConnection.asyncSend(nicknameSetMessage);
     }
 
+    /**
+     * @param numberOfPlayersSetMessage
+     * Update method to observe the userInterface. It forwards the request to the network handler
+     */
     @Override
     public void update(NumberOfPlayersSetMessage numberOfPlayersSetMessage) {
         serverConnection.asyncSend(numberOfPlayersSetMessage);
     }
 
+    /**
+     * @param selectedPawnSetMessage
+     * Update method to observe the userInterface. It forwards the request to the network handler
+     */
     @Override
     public void update(SelectedPawnSetMessage selectedPawnSetMessage) {
         serverConnection.asyncSend(selectedPawnSetMessage);
     }
 
+    /**
+     * @param undoTurnSetMessage
+     * Update method to observe the userInterface. It forwards the request to the network handler
+     */
     @Override
     public void update(UndoTurnSetMessage undoTurnSetMessage) {
         serverConnection.asyncSend(undoTurnSetMessage);
     }
 
+    /**
+     * @param undoActionSetMessage
+     * Update method to observe the userInterface. It forwards the request to the network handler
+     */
     @Override
     public void update(UndoActionSetMessage undoActionSetMessage) {
         serverConnection.asyncSend(undoActionSetMessage);
