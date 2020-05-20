1ecf7b7be792bf99b988be26b11e5749a52869ea
Luca Danelutti <lucadanelutti@outlook.com>
2020-05-20 12:04:14 +0200

    Improve JavaDoc

--- src/main/java/it/polimi/ingsw/view/VirtualView.java
+++ src/main/java/it/polimi/ingsw/view/VirtualView.java
@@ -8,83 +8,152 @@ import it.polimi.ingsw.utility.messages.updates.*;
 import it.polimi.ingsw.view.listeners.RequestsAndUpdateListener;
 import it.polimi.ingsw.view.listeners.SetsListener;
 
+/**
+ * VirtualView class: each player has his own virtualView on the server.
+ * Every virtualView is observed by the controller and it observes the model for updates and the network handler for sets
+ */
 public class VirtualView extends SetObservable implements RequestsAndUpdateListener, SetsListener{
     final private ClientConnection clientConnection;
     final private String name;
 
+    /**
+     * @param chosenBlockTypeSetMessage
+     * Update method to observe the network handler. The method sets the name of the sender
+     * and it forwards the request to the controller with observable notifyListeners method
+     */
     @Override
     public void update(ChosenBlockTypeSetMessage chosenBlockTypeSetMessage) {
         chosenBlockTypeSetMessage.setNameOfTheSender(name);
         notifyListeners(chosenBlockTypeSetMessage);
     }
 
+    /**
+     * @param chosenCardSetMessage
+     * Update method to observe the network handler. The method sets the name of the sender
+     * and it forwards the request to the controller with observable notifyListeners method
+     */
     @Override
     public void update(ChosenCardSetMessage chosenCardSetMessage) {
         chosenCardSetMessage.setNameOfTheSender(name);
         notifyListeners(chosenCardSetMessage);
     }
 
+    /**
+     * @param chosenPositionSetMessage
+     * Update method to observe the network handler. The method sets the name of the sender
+     * and it forwards the request to the controller with observable notifyListeners method
+     */
     @Override
     public void update(ChosenPositionSetMessage chosenPositionSetMessage) {
         chosenPositionSetMessage.setNameOfTheSender(name);
         notifyListeners(chosenPositionSetMessage);
     }
 
+    /**
+     * @param firstPlayerSetMessage
+     * Update method to observe the network handler. The method sets the name of the sender
+     * and it forwards the request to the controller with observable notifyListeners method
+     */
     @Override
     public void update(FirstPlayerSetMessage firstPlayerSetMessage) {
         firstPlayerSetMessage.setNameOfTheSender(name);
         notifyListeners(firstPlayerSetMessage);
     }
 
+    /**
+     * @param inGameCardsSetMessage
+     * Update method to observe the network handler. The method sets the name of the sender
+     * and it forwards the request to the controller with observable notifyListeners method
+     */
     @Override
     public void update(InGameCardsSetMessage inGameCardsSetMessage) {
         inGameCardsSetMessage.setNameOfTheSender(name);
         notifyListeners(inGameCardsSetMessage);
     }
 
+    /**
+     * @param initialPawnPositionSetMessage
+     * Update method to observe the network handler. The method sets the name of the sender
+     * and it forwards the request to the controller with observable notifyListeners method
+     */
     @Override
     public void update(InitialPawnPositionSetMessage initialPawnPositionSetMessage) {
         initialPawnPositionSetMessage.setNameOfTheSender(name);
         notifyListeners(initialPawnPositionSetMessage);
     }
 
+    /**
+     * @param nicknameSetMessage
+     * Update method to observe the network handler. The method sets the name of the sender
+     * and it forwards the request to the controller with observable notifyListeners method
+     */
     @Override
     public void update(NicknameSetMessage nicknameSetMessage) {
         nicknameSetMessage.setNameOfTheSender(name);
         notifyListeners(nicknameSetMessage);
     }
 
+    /**
+     * @param numberOfPlayersSetMessage
+     * Update method to observe the network handler. The method sets the name of the sender
+     * and it forwards the request to the controller with observable notifyListeners method
+     */
     @Override
     public void update(NumberOfPlayersSetMessage numberOfPlayersSetMessage) {
         numberOfPlayersSetMessage.setNameOfTheSender(name);
         notifyListeners(numberOfPlayersSetMessage);
     }
 
+    /**
+     * @param selectedPawnSetMessage
+     * Update method to observe the network handler. The method sets the name of the sender
+     * and it forwards the request to the controller with observable notifyListeners method
+     */
     @Override
     public void update(SelectedPawnSetMessage selectedPawnSetMessage) {
         selectedPawnSetMessage.setNameOfTheSender(name);
         notifyListeners(selectedPawnSetMessage);
     }
 
+    /**
+     * @param undoTurnSetMessage
+     * Update method to observe the network handler. The method sets the name of the sender
+     * and it forwards the request to the controller with observable notifyListeners method
+     */
     @Override
     public void update(UndoTurnSetMessage undoTurnSetMessage) {
         undoTurnSetMessage.setNameOfTheSender(name);
         notifyListeners(undoTurnSetMessage);
     }
 
+    /**
+     * @param undoActionSetMessage
+     * Update method to observe the network handler. The method sets the name of the sender
+     * and it forwards the request to the controller with observable notifyListeners method
+     */
     @Override
     public void update(UndoActionSetMessage undoActionSetMessage) {
         undoActionSetMessage.setNameOfTheSender(name);
         notifyListeners(undoActionSetMessage);
     }
 
-    public VirtualView(ClientConnection c, String name) {
-        this.clientConnection = c;
+    /**
+     * @param clientConnection: reference to the network handler object to send data
+     * @param name: name of the player linked to this virtualView
+     * Default constructor for the VirtualView class
+     */
+    public VirtualView(ClientConnection clientConnection, String name) {
+        this.clientConnection = clientConnection;
         this.name = name;
-        c.addListener(this);
+        clientConnection.addListener(this);
         System.out.println(name + ": virtualView created!");    //TODO: logging
     }
 
+    /**
+     * @param nicknameRequestMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(NicknameRequestMessage nicknameRequestMessage) {
         if (nicknameRequestMessage.getRecipients().contains(name)) {
@@ -92,6 +161,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
+    /**
+     * @param chosenBlockTypeRequestMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(ChosenBlockTypeRequestMessage chosenBlockTypeRequestMessage) {
         if (chosenBlockTypeRequestMessage.getRecipients().contains(name)) {
@@ -99,6 +173,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
+    /**
+     * @param chosenCardRequestMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(ChosenCardRequestMessage chosenCardRequestMessage) {
         if (chosenCardRequestMessage.getRecipients().contains(name)) {
@@ -106,6 +185,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
+    /**
+     * @param chosenPositionForMoveRequestMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(ChosenPositionForMoveRequestMessage chosenPositionForMoveRequestMessage) {
         if (chosenPositionForMoveRequestMessage.getRecipients().contains(name)) {
@@ -113,6 +197,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
+    /**
+     * @param chosenPositionForConstructRequestMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(ChosenPositionForConstructRequestMessage chosenPositionForConstructRequestMessage) {
         if (chosenPositionForConstructRequestMessage.getRecipients().contains(name)) {
@@ -120,6 +209,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
+    /**
+     * @param firstPlayerRequestMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(FirstPlayerRequestMessage firstPlayerRequestMessage) {
         if (firstPlayerRequestMessage.getRecipients().contains(name)) {
@@ -127,6 +221,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
+    /**
+     * @param inGameCardsRequestMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(InGameCardsRequestMessage inGameCardsRequestMessage) {
         if (inGameCardsRequestMessage.getRecipients().contains(name)) {
@@ -134,6 +233,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
+    /**
+     * @param initialPawnPositionRequestMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(InitialPawnPositionRequestMessage initialPawnPositionRequestMessage) {
         if (initialPawnPositionRequestMessage.getRecipients().contains(name)) {
@@ -141,6 +245,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
+    /**
+     * @param numberOfPlayersRequestMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(NumberOfPlayersRequestMessage numberOfPlayersRequestMessage) {
         if (numberOfPlayersRequestMessage.getRecipients().contains(name)) {
@@ -148,6 +257,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
+    /**
+     * @param selectPawnRequestMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(SelectPawnRequestMessage selectPawnRequestMessage) {
         if (selectPawnRequestMessage.getRecipients().contains(name)) {
@@ -155,6 +269,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
+    /**
+     * @param cellUpdateMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(CellUpdateMessage cellUpdateMessage) {
         if (cellUpdateMessage.getRecipients().contains(name)) {
@@ -162,6 +281,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
+    /**
+     * @param chosenCardUpdateMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(ChosenCardUpdateMessage chosenCardUpdateMessage) {
         if (chosenCardUpdateMessage.getRecipients().contains(name)) {
@@ -169,6 +293,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
+    /**
+     * @param doublePawnPositionUpdateMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(DoublePawnPositionUpdateMessage doublePawnPositionUpdateMessage) {
         if (doublePawnPositionUpdateMessage.getRecipients().contains(name)) {
@@ -176,6 +305,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
+    /**
+     * @param pawnPositionUpdateMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(PawnPositionUpdateMessage pawnPositionUpdateMessage) {
         if (pawnPositionUpdateMessage.getRecipients().contains(name)) {
@@ -183,6 +317,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
+    /**
+     * @param pawnRemoveUpdateMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(PawnRemoveUpdateMessage pawnRemoveUpdateMessage) {
         if (pawnRemoveUpdateMessage.getRecipients().contains(name)) {
@@ -190,7 +329,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
-
+    /**
+     * @param selectedPawnUpdateMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(SelectedPawnUpdateMessage selectedPawnUpdateMessage) {
         if (selectedPawnUpdateMessage.getRecipients().contains(name)) {
@@ -198,6 +341,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
+    /**
+     * @param gameStartMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(GameStartMessage gameStartMessage) {
         if (gameStartMessage.getRecipients().contains(name)) {
@@ -205,6 +353,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
+    /**
+     * @param gameEndedMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(GameEndedMessage gameEndedMessage) {
         if (gameEndedMessage.getRecipients().contains(name)) {
@@ -212,6 +365,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
+    /**
+     * @param turnEndedMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(TurnEndedMessage turnEndedMessage) {
         if (turnEndedMessage.getRecipients().contains(name)) {
@@ -219,6 +377,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
+    /**
+     * @param youLostMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(YouLostMessage youLostMessage) {
         if (youLostMessage.getRecipients().contains(name)) {
@@ -226,6 +389,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
+    /**
+     * @param youLostAndSomeoneWonMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(YouLostAndSomeoneWonMessage youLostAndSomeoneWonMessage) {
         if (youLostAndSomeoneWonMessage.getRecipients().contains(name)) {
@@ -233,6 +401,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
+    /**
+     * @param youWonMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(YouWonMessage youWonMessage) {
         if (youWonMessage.getRecipients().contains(name)) {
@@ -240,6 +413,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
+    /**
+     * @param gameStartedAndYouAreNotSelectedMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(gameStartedAndYouAreNotSelectedMessage gameStartedAndYouAreNotSelectedMessage) {
         if (gameStartedAndYouAreNotSelectedMessage.getRecipients().contains(name)) {
@@ -247,6 +425,11 @@ public class VirtualView extends SetObservable implements RequestsAndUpdateListe
         }
     }
 
+    /**
+     * @param undoUpdateMessage
+     * Update method to observe the model. The method checks that the message has to be sent to the
+     * player linked to this virtualView and then it forwards the request to the network handler
+     */
     @Override
     public void update(UndoUpdateMessage undoUpdateMessage) {
         if (undoUpdateMessage.getRecipients().contains(name)) {
