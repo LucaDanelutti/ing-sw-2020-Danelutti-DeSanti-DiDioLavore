b3aa3d4e44a9d8ec253a226d66a0047d124e29f3
Luca Danelutti <lucadanelutti@outlook.com>
2020-05-31 21:02:28 +0200

    Fix logs folder creation bug

--- src/main/java/it/polimi/ingsw/utility/MyLogger.java
+++ src/main/java/it/polimi/ingsw/utility/MyLogger.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.utility;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.NoSuchFileException;
 import java.util.logging.*;
 
 public class MyLogger {
@@ -14,8 +16,23 @@ public class MyLogger {
                 "[%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS] [%4$s]: %5$s%n");
         //instance the logger
         logger = Logger.getLogger(MyLogger.class.getName());
-        //instance the file handler
-        fileHandler = new FileHandler("logs/serverLog.txt", true);
+        try {
+            //instance the file handler
+            fileHandler = new FileHandler("logs/serverLog.txt", true);
+        } catch(NoSuchFileException ex) {
+            //if parent directory does not exist then attempt to create it
+            String parentName = new File("logs/serverLog.txt").getParent();
+            if (parentName != null) {
+                File parentDir = new File(parentName);
+                if(!parentDir.exists() && parentDir.mkdirs()) {
+                    fileHandler = new FileHandler("logs/serverLog.txt", true);
+                } else {
+                    throw ex;
+                }
+        } else {
+            throw ex;
+        }
+    }
         //instance formatter, set formatting, and handler
         plainText = new SimpleFormatter();
         fileHandler.setFormatter(plainText);
